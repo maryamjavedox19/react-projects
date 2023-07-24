85014de3c1e76a2958176449a4d79e3b081fd4e9
Maryam <maryamjaved.xo@gmail.com>
2023-07-24 15:00:51 +0500

    styling and media query

--- src/components/Section.js
+++ src/components/Section.js
@@ -18,7 +18,6 @@ function Section() {
 
                 <DownArrow src="images/down-arrow.svg"></DownArrow>
             </Buttons>
-
         </Wrap>
     );
 }
@@ -31,10 +30,10 @@ const Wrap = styled.div`
   background-position: center;
   background-repeat: no-repeat;
   background-image: url("/images/model-s.jpg");
-  display:flex;
-  flex-direction:column;
-  justify-content:space-between;  
-  align-items:center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const ItemText = styled.div`
@@ -44,7 +43,10 @@ const ItemText = styled.div`
 
 const ButtonGroup = styled.div`
   display: flex;
-  margin-bottom:30px;
+  margin-bottom: 30px;
+  @media(max-width:768px){
+    flex-direction:column;
+  }
 `;
 
 const LeftButton = styled.div`
@@ -59,20 +61,21 @@ const LeftButton = styled.div`
   opacity: 0.85;
   text-transform: uppercase;
   font-size: 12px;
-  cursor:pointer;
-  margin:8px;
+  cursor: pointer;
+  margin: 8px;
 `;
 
 // it allows us to hear the all css of left button
-const RightButton = styled(LeftButton)``;
-
-
+const RightButton = styled(LeftButton)`
+  background: white;
+  opacity: 0.65;
+  color: black;
+`;
 
 const DownArrow = styled.img`
-margin-top:20px;
-height:40px;
-overflow-x:hidden;
-animation: animateDown infinite 1.5s;
-`
+  height: 40px;
+  overflow-x: hidden;
+  animation: animateDown infinite 1.5s;
+`;
 
-const Buttons = styled.div``
+const Buttons = styled.div``;
